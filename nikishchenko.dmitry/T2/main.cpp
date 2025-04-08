3d3546c1de57eb7368563cda351a3c6116d9b03c
dimene <ugrumydm@gmail.com>
2025-04-09 02:41:02 +0300

    Removed cleanup from main()

--- nikishchenko.dmitry/T2/main.cpp
+++ nikishchenko.dmitry/T2/main.cpp
@@ -12,15 +12,11 @@ void cleanVector(std::vector<DataStruct>& vector);
 int main()
 {
     std::vector<DataStruct> dataVector;
-    while (!(std::cin.eof()))
-    {
     std::copy(
         std::istream_iterator<DataStruct>(std::cin),
         std::istream_iterator<DataStruct>(),
         std::back_inserter(dataVector)
     );
-    }
-    cleanVector(dataVector);
     std::sort(
         dataVector.begin(),
         dataVector.end(),
