68a18b1a309f3bc409e7d3a355ff591bcdd4e82a
Timur Mironchuk <tima888112@gmail.com>
2025-04-23 11:03:26 +0300

    добавил константы, немного поменял логику

--- mironchuk.timur/T2/main.cpp
+++ mironchuk.timur/T2/main.cpp
@@ -4,39 +4,41 @@
 #include <algorithm>
 #include <stdexcept>
 
-const std::string ERROR_OF_EMPTY_VECTOR =
-        "Looks like there is no supported record. Cannot determine input. Test skipped";
+const std::string NO_INPUT_MSG = "Looks like there is no supported record. Cannot determine input. Test skipped";
+const std::string ATLEAST_ONE_MSG = "Atleast one supported record type";
 
 int main() {
     std::vector<DataStruct> vector;
-    bool has_input = false;
+    bool has_any_input = false;
+    bool has_valid_records = false;
 
     while (!std::cin.eof()) {
-        std::copy(
-            std::istream_iterator<DataStruct>(std::cin),
-            std::istream_iterator<DataStruct>(),
-            std::back_inserter(vector)
-        );
-        has_input = has_input || !vector.empty();
+        DataStruct temp;
+        std::cin >> temp;
 
         if (std::cin.fail()) {
+            has_any_input = true;
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            has_any_input = true;
+            has_valid_records = true;
+            vector.push_back(temp);
         }
     }
 
-    if (!has_input) {
-        std::cout << ERROR_OF_EMPTY_VECTOR << std::endl;
+    if (!has_any_input) {
+        std::cout << NO_INPUT_MSG << "\n";
         return 0;
     }
 
-    if (vector.empty()) {
-        std::cout << "Atleast one supported record type" << std::endl;
+    if (!has_valid_records) {
+        std::cout << ATLEAST_ONE_MSG << "\n";
         return 0;
     }
 
     if (vector.size() == 1) {
-        std::cout << "Atleast one supported record type" << std::endl;
+        std::cout << ATLEAST_ONE_MSG << "\n";
         return 0;
     }
 
