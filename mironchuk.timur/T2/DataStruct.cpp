73f6f5d0c68d4cea281157754d18ed028340bbb9
Timur Mironchuk <tima888112@gmail.com>
2025-04-23 08:57:08 +0300

    добавил перенос строки в сообщениях

--- mironchuk.timur/T2/DataStruct.cpp
+++ mironchuk.timur/T2/DataStruct.cpp
@@ -18,42 +18,28 @@ std::vector<DataStruct> parseData(std::istream &in) {
 
     while (std::getline(in, line)) {
         has_input = true;
-
-        line.erase(line.begin(), std::find_if(line.begin(), line.end(), [](int ch) {
-            return !std::isspace(ch);
-        }));
-        line.erase(std::find_if(line.rbegin(), line.rend(), [](int ch) {
-            return !std::isspace(ch);
-        }).base(), line.end());
-
+        // trim
+        line.erase(line.begin(), std::find_if(line.begin(), line.end(), [](int ch) { return !std::isspace(ch); }));
+        line.erase(std::find_if(line.rbegin(), line.rend(), [](int ch) { return !std::isspace(ch); }).base(), line.end());
         if (line.empty()) continue;
+        if (line.size() < 4 || line.front() != '(' || line.back() != ')') continue;
 
-        if (line.size() < 4 || line.front() != '(' || line.back() != ')') {
-            continue;
-        }
-
-        DataStruct ds;
+        DataStruct ds{0, {0,0}, ""};
         bool hasKey1 = false, hasKey2 = false, hasKey3 = false;
         std::string content = line.substr(1, line.size() - 2);
-
         size_t pos = 0;
         while (pos < content.size()) {
             size_t colon = content.find(':', pos);
             if (colon == std::string::npos) break;
-
             std::string field = content.substr(pos, colon - pos);
             pos = colon + 1;
-
-            if (field.find("key1 ") == 0 && !hasKey1) {
+            if (field.rfind("key1 ", 0) == 0 && !hasKey1) {
                 std::string value = field.substr(5);
-                try {
-                    if (value[0] == '0' && value.find_first_not_of("01234567") == std::string::npos) {
-                        ds.key1 = std::stoull(value, nullptr, 8);
-                        hasKey1 = true;
-                    }
-                } catch (...) {
+                if (value.size() >= 2 && value[0] == '0' && value.find_first_not_of("01234567") == std::string::npos) {
+                    ds.key1 = std::stoull(value, nullptr, 8);
+                    hasKey1 = true;
                 }
-            } else if (field.find("key2 #c(") == 0 && !hasKey2) {
+            } else if (field.rfind("key2 #c(", 0) == 0 && !hasKey2) {
                 size_t end = content.find(')', pos);
                 if (end != std::string::npos) {
                     std::string nums = content.substr(pos, end - pos);
@@ -65,7 +51,7 @@ std::vector<DataStruct> parseData(std::istream &in) {
                     }
                     pos = end + 1;
                 }
-            } else if (field.find("key3 \"") == 0 && !hasKey3) {
+            } else if (field.rfind("key3 \"", 0) == 0 && !hasKey3) {
                 size_t end = content.find('"', pos);
                 if (end != std::string::npos) {
                     ds.key3 = content.substr(pos, end - pos);
@@ -74,16 +60,11 @@ std::vector<DataStruct> parseData(std::istream &in) {
                 }
             }
         }
-
         if (hasKey1 && hasKey2 && hasKey3) {
             data.push_back(ds);
         }
     }
-
-    if (has_input && data.empty()) {
-        data.push_back({0, {0, 0}, ""});
-    }
-
+    if (!data.empty() || !has_input) return data;
     return data;
 }
 
@@ -92,10 +73,12 @@ void sortData(std::vector<DataStruct> &data) {
 }
 
 void printData(const std::vector<DataStruct> &data, std::ostream &out) {
-    for (const auto &ds: data) {
-        out << "(:key1 0" << std::oct << ds.key1 << std::dec;
+    for (const auto &ds : data) {
+        out << "(:key1 " << std::oct << ds.key1 << std::dec;
         out << ":key2 #c(" << ds.key2.real() << " " << ds.key2.imag() << ")";
-        out << ":key3 \"" << ds.key3 << "\":)\n";
+        out << ":key3 \"" << ds.key3 << "\":)";
+        out << '\n';
     }
 }
+
 
--- mironchuk.timur/T2/main.cpp
+++ mironchuk.timur/T2/main.cpp
@@ -3,22 +3,14 @@
 
 int main() {
     auto data = parseData(std::cin);
-
     if (data.empty()) {
-        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped";
+        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped" << std::endl;
         return 0;
     }
-
-    if (data.size() == 1 && data[0].key3.empty()) {
-        std::cout << "Atleast one supported record type";
-        return 0;
-    }
-
     if (data.size() == 1) {
-        std::cout << "Atleast one supported record type";
+        std::cout << "Atleast one supported record type" << std::endl;
         return 0;
     }
-
     sortData(data);
     printData(data, std::cout);
     return 0;
