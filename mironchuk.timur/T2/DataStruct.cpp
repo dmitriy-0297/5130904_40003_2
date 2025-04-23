ae88446e501e7619f76fee25d3583da10f25aa9f
Timur Mironchuk <tima888112@gmail.com>
2025-04-23 07:32:11 +0300

    spaces fix

--- mironchuk.timur/T2/DataStruct.cpp
+++ mironchuk.timur/T2/DataStruct.cpp
@@ -2,20 +2,19 @@
 #include <algorithm>
 #include <iterator>
 #include <stdexcept>
-#include <cmath>
 #include <sstream>
 #include <cctype>
 
 std::size_t total_lines_read = 0;
 
-static bool compareData(const DataStruct& a, const DataStruct& b) {
+static bool compareData(const DataStruct &a, const DataStruct &b) {
     if (a.key1 != b.key1) return a.key1 < b.key1;
     double magA = std::abs(a.key2), magB = std::abs(b.key2);
     if (magA != magB) return magA < magB;
     return a.key3.length() < b.key3.length();
 }
 
-std::vector<DataStruct> parseData(std::istream& in) {
+std::vector<DataStruct> parseData(std::istream &in) {
     std::vector<DataStruct> data;
     std::string line;
     total_lines_read = 0;
@@ -24,7 +23,8 @@ std::vector<DataStruct> parseData(std::istream& in) {
         ++total_lines_read;
 
         line.erase(line.begin(), std::find_if(line.begin(), line.end(), [](int ch) { return !std::isspace(ch); }));
-        line.erase(std::find_if(line.rbegin(), line.rend(), [](int ch) { return !std::isspace(ch); }).base(), line.end());
+        line.erase(std::find_if(line.rbegin(), line.rend(), [](int ch) { return !std::isspace(ch); }).base(),
+                   line.end());
 
         if (line.size() < 4 || line.front() != '(' || line.back() != ')') {
             continue;
@@ -39,7 +39,7 @@ std::vector<DataStruct> parseData(std::istream& in) {
         while (start < content.size()) {
             size_t end = content.find(':', start);
             if (end == std::string::npos) end = content.size();
-            
+
             std::string field = content.substr(start, end - start);
             if (!field.empty()) {
                 if (!hasKey1 && field.find("key1 ") == 0) {
@@ -49,9 +49,9 @@ std::vector<DataStruct> parseData(std::istream& in) {
                             ds.key1 = std::stoull(value, nullptr, 8);
                             hasKey1 = true;
                         }
-                    } catch (...) {}
-                }
-                else if (!hasKey2 && field.find("key2 #c(") == 0) {
+                    } catch (...) {
+                    }
+                } else if (!hasKey2 && field.find("key2 #c(") == 0) {
                     size_t end_paren = field.rfind(')');
                     if (end_paren != std::string::npos) {
                         std::string nums = field.substr(7, end_paren - 7);
@@ -62,8 +62,7 @@ std::vector<DataStruct> parseData(std::istream& in) {
                             hasKey2 = true;
                         }
                     }
-                }
-                else if (!hasKey3 && field.find("key3 \"") == 0) {
+                } else if (!hasKey3 && field.find("key3 \"") == 0) {
                     size_t end_quote = field.rfind('"');
                     if (end_quote != std::string::npos && end_quote > 6) {
                         ds.key3 = field.substr(6, end_quote - 6);
@@ -71,7 +70,7 @@ std::vector<DataStruct> parseData(std::istream& in) {
                     }
                 }
             }
-            
+
             start = end + 1;
         }
 
@@ -82,17 +81,18 @@ std::vector<DataStruct> parseData(std::istream& in) {
     return data;
 }
 
-void sortData(std::vector<DataStruct>& data) {
+void sortData(std::vector<DataStruct> &data) {
     std::sort(data.begin(), data.end(), compareData);
 }
 
-void printData(const std::vector<DataStruct>& data, std::ostream& out) {
+void printData(const std::vector<DataStruct> &data, std::ostream &out) {
     std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(out, "\n"));
 }
 
-std::ostream& operator<<(std::ostream& out, const DataStruct& ds) {
+std::ostream &operator<<(std::ostream &out, const DataStruct &ds) {
     out << "(:key1 0" << std::oct << ds.key1 << std::dec;
     out << ":key2 #c(" << ds.key2.real() << " " << ds.key2.imag() << ")";
     out << ":key3 \"" << ds.key3 << "\":)";
     return out;
 }
+
--- mironchuk.timur/T2/DataStruct.h
+++ mironchuk.timur/T2/DataStruct.h
@@ -16,9 +16,13 @@ struct DataStruct {
 
 extern std::size_t total_lines_read;
 
-std::vector<DataStruct> parseData(std::istream& in);
-void sortData(std::vector<DataStruct>& data);
-void printData(const std::vector<DataStruct>& data, std::ostream& out);
-std::ostream& operator<<(std::ostream& out, const DataStruct& ds);
+std::vector<DataStruct> parseData(std::istream &in);
+
+void sortData(std::vector<DataStruct> &data);
+
+void printData(const std::vector<DataStruct> &data, std::ostream &out);
+
+std::ostream &operator<<(std::ostream &out, const DataStruct &ds);
 
 #endif // DATASTRUCT_H
+
