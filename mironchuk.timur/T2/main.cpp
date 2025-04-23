db42fc8a7b6fb0b37d9d615d169ab16dccf9770d
Timur Mironchuk <tima888112@gmail.com>
2025-04-23 10:19:57 +0300

    исправил ввод atleast one...

--- mironchuk.timur/T2/main.cpp
+++ mironchuk.timur/T2/main.cpp
@@ -3,11 +3,14 @@
 
 int main() {
     auto data = parseData(std::cin);
-
-    if (data.empty()) {
+    
+    std::istreambuf_iterator<char> it(std::cin), end;
+    bool no_input = (it == end);
+    if (no_input) {
         std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped\n";
         return 0;
     }
+
     if (data.size() == 1) {
         std::cout << "Atleast one supported record type\n";
         return 0;
