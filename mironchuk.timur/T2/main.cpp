bfa4225c94c067a64abcf4ff7b3df9678e82634b
Timur Mironchuk <tima888112@gmail.com>
2025-04-23 07:05:46 +0300

    big fix

--- mironchuk.timur/T2/main.cpp
+++ mironchuk.timur/T2/main.cpp
@@ -3,14 +3,17 @@
 
 int main() {
     auto data = parseData(std::cin);
+
     if (total_lines_read == 0) {
         std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped";
         return 0;
     }
+
     if (data.size() < 2) {
         std::cout << "Atleast one supported record type";
         return 0;
     }
+
     sortData(data);
     printData(data, std::cout);
     return 0;
