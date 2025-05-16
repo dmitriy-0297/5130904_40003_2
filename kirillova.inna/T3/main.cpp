ed5be67359a563cbc0907d04336f388d03195e16
kirinnah <inna.kirillova005@gmail.com>
2025-05-16 04:06:46 +0300

    Try to fix COUNT by changing input

--- kirillova.inna/T3/main.cpp
+++ kirillova.inna/T3/main.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <iterator>
-#include <sstream>
 
 #include "Polygons.h"
 #include "Commands.h"
@@ -13,53 +10,69 @@ const std::string ERROR_OF_READING_FILE = "ERROR: can't read the file!";
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2)
+  if (argc != 2)
+  {
+    std::cerr << ERROR_WRONG_NUMBER_OF_ARGUMENTS << "\n";
+    return EXIT_FAILURE;
+  }
+
+  std::ifstream file(argv[1]);
+
+  if (!file)
+  {
+    std::cerr << ERROR_OF_OPENING_FILE << "\n";
+    return EXIT_FAILURE;
+  }
+
+  using input_it = std::istream_iterator<kirillova::Polygon>;
+  std::vector<kirillova::Polygon> polygons;
+
+  std::string line;
+  while (std::getline(file, line))
+  {
+    std::istringstream iss(line);
+    kirillova::Polygon poly;
+    if (iss >> poly)
     {
-        std::cerr << ERROR_WRONG_NUMBER_OF_ARGUMENTS << "\n";
-        return EXIT_FAILURE;
+      polygons.push_back(poly);
+    }
+  }
+
+  if (file.bad())
+  {
+    std::cerr << ERROR_OF_READING_FILE << "\n";
+    return EXIT_FAILURE;
+  }
+
+  if (file.fail() && !file.eof())
+  {
+    std::cerr << ERROR_OF_READING_FILE << "\n";
+    return EXIT_FAILURE;
+  }
+
+  std::string commandLine;
+  while (std::getline(std::cin, commandLine))
+  {
+    std::istringstream iss(commandLine);
+    std::string command;
+    std::string arguments;
+
+    iss >> command;
+    std::getline(iss, arguments);
+    if (!arguments.empty() && arguments.front() == ' ')
+    {
+      arguments.erase(0, 1);
     }
 
-    std::ifstream file(argv[1]);
-    if (!file)
+    try
     {
-        std::cerr << ERROR_OF_OPENING_FILE << "\n";
-        return EXIT_FAILURE;
+      executeCommands(polygons, command, arguments);
     }
-
-    using input_it = std::istream_iterator<kirillova::Polygon>;
-    std::vector<kirillova::Polygon> polygons;
-
-    std::copy(input_it{ file }, input_it{}, std::back_inserter(polygons));
-
-    if (file.bad())
+    catch (const std::invalid_argument& except)
     {
-        std::cerr << ERROR_OF_READING_FILE << "\n";
-        return EXIT_FAILURE;
+      std::cerr << ERROR_OF_WRONG_COMMAND << "\n";
     }
+  }
 
-    std::string commandLine;
-    while (std::getline(std::cin, commandLine))
-    {
-        std::istringstream iss(commandLine);
-        std::string command;
-        std::string arguments;
-
-        iss >> command;
-        std::getline(iss, arguments);
-        if (!arguments.empty() && arguments.front() == ' ')
-        {
-            arguments.erase(0, 1);
-        }
-
-        try
-        {
-            executeCommands(polygons, command, arguments);
-        }
-        catch (const std::invalid_argument&)
-        {
-            std::cerr << ERROR_OF_WRONG_COMMAND << "\n";
-        }
-    }
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
