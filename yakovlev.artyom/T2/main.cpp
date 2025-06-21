13d691b86fa20c1d07f5db00baac4fb19c3ce197
nxgmvw <artem-tem00@inbox.ru>
2025-06-21 14:42:33 +0300

    fix

--- yakovlev.artyom/T2/main.cpp
+++ yakovlev.artyom/T2/main.cpp
@@ -1,22 +1,39 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <vector>
 
 #include "Data_struct.h"
 
 int main()
 {
-    std::vector<yakovlevart::DataStruct> data;
+    using yakovlevart::DataStruct;
+    std::vector<DataStruct> data;
 
-    std::copy(std::istream_iterator<yakovlevart::DataStruct>{std::cin},
-        std::istream_iterator<yakovlevart::DataStruct>{},
-        std::back_inserter(data));
+    DataStruct tmp;
+    while (true)
+    {
+        if (std::cin >> tmp)
+        {
+            data.push_back(tmp);
+        }
+        else if (std::cin.eof())
+        {
+            break;
+        }
+        else
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), ')');
+            continue;
+        }
+    }
 
     std::sort(data.begin(), data.end());
 
     std::copy(data.begin(), data.end(),
-        std::ostream_iterator<yakovlevart::DataStruct>{std::cout, "\n"});
+        std::ostream_iterator<DataStruct>{std::cout, "\n"});
 
     return 0;
 }
